fa40424ed1b76b96d3318cee974b0777151a14bd
DimonBel <144515910+DimonBel@users.noreply.github.com>
2024-10-03 00:27:33 +0300

    Resolved conflict in page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// app/dashboard/page.tsx
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {/* Add more dashboard content here */}
+            saddsdsadsadsasd
+        </div>
+    );
+};
+
+export default DashboardPage;
