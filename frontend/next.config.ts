988c01450b65c798ace299d7b597d5d233b07571
DimonBel <144515910+DimonBel@users.noreply.github.com>
2025-07-06 23:00:20 +0300

    Add component dashboard

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['i.postimg.cc', 'learnloner.com', 'www.intervolga.ru'],
+  },
 };
 
 export default nextConfig;
---
+++ frontend/src/components/custom/find-job.tsx
@@ -0,0 +1,13 @@
+
+// // Import the CSS file
+// import '../../app/globals.css';
+// import { FindJobCard } from "@/components/custom/card-job"
+// import { Card } from '../ui/card';
+// export function FindJob() {
+
+//     return (
+//         <FindJobCard />
+
+//     );
+// }
+
